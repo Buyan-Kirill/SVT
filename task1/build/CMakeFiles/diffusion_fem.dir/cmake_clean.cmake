d3162200b8b407b223ff3c5c4d36b53a37700764
Buyan-Kirill <kiruhabuyan@gmail.com>
2024-12-24 15:28:10 +0300

    Ahmat and Anna are the best

---
+++ task1/build/CMakeFiles/diffusion_fem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/diffusion_fem.dir/diffusion_fem.cpp.o"
+  "CMakeFiles/diffusion_fem.dir/diffusion_fem.cpp.o.d"
+  "diffusion_fem"
+  "diffusion_fem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/diffusion_fem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
